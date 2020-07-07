034ed87914d6b78fa68ffb09977ff7534bd4884e
Elen <petrielen@outlook.com>
2020-07-07 15:41:35 -0300

    Alterado nossa Rest API para realzar autenticação no banco de dados. Em models foi criado tabela de usuários com login e senha para autenticação nas APIs. Incuido em requirements a extensão flask-httpauth

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
-from models import Pessoas
-
+from models import Pessoas, Usuarios
+from models import Atividades
 
 
 def insere_pessoas():
-    pessoa = Pessoas(nome='Elen', idade=26)
+    pessoa = Pessoas(nome='Fernanda', idade=27)
     print(pessoa)
     pessoa.save()
 
@@ -23,11 +23,33 @@ def altera_pessoas():
     pessoa.save()
 
 def exclui_pessoas():
-    pessoa = Pessoas.query.filter_by(nome='Elen').first()
+    pessoa = Pessoas.query.filter_by(nome='Fernada').first()
     pessoa.delete()
 
+def insere_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+def consulta_todos_usuarios():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
+
+def exclui_usuarios():
+    usuario = Usuarios.query.filter_by(login='felipe').first()
+    usuario.delete()
+
+def consulta_atividade():
+    atividades = Atividades.query.all()
+    print(atividades)
+
 if __name__ == '__main__':
+    # insere_usuario('giovanna', '1234')
+    # insere_usuario('felipe', '4321')
+    # exclui_usuarios()
+    consulta_todos_usuarios()
     # insere_pessoas()
-    consulta_pessoas()
-    exclui_pessoas()
+    # consulta_atividade()
+    # exclui_pessoas()
     # altera_pessoas()
+
+
